a9bd7cd6d9d86acf9f4af1678642d9717261f096
Felipe <felipe.matheussms@gmail.com>
2025-05-10 15:48:48 -0300

    criando a classe Habilidades

---
+++ src/br/com/voca/modelos/Habilidades.java
@@ -0,0 +1,31 @@
+package br.com.voca.modelos;
+
+public class Habilidades {
+    private String habilidade; //atributo para armazenar a habilidade do usuário
+    private Nivel nivel; //atributo utilizando Enum para o atributo receber valores fixos pré-determinados
+    private String nivelStr; //String para armazenar a situação e ser convertida para o Enum
+
+    //enumeração de valores fixos (constantes) para a situação do curso utilizando a classe Enum
+    public enum Nivel {
+        BASICO,
+        INTERMEDIARIO,
+        AVANCADO
+    }
+
+    //setter da habilidade com validação contra string nula ou vazia
+    public void setHabilidade(String habilidade) {
+        if (habilidade != null && !habilidade.isBlank()) {
+            this.habilidade = habilidade;
+        }
+    }
+
+    /*setter do nivel de habilidade. recebe uma string, deixa-a em letras maiúsculas e converte para o tipo Enum e atribui
+    ao atributo Enum nivel*/
+    public void setNivel(String nivelStr) {
+        try {
+            this.nivel = Nivel.valueOf(nivelStr.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            //implementar tratamento de exceção no front-end
+        }
+    }
+}
