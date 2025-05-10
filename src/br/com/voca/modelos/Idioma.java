3b78b46ede453c613dd493006dc1b45e058c9cd2
Felipe <felipe.matheussms@gmail.com>
2025-05-10 15:51:46 -0300

    comentando a classe idioma

--- src/br/com/voca/modelos/Idioma.java
+++ src/br/com/voca/modelos/Idioma.java
@@ -5,26 +5,32 @@ public class Idioma {
     private String nivelStr;
     private Nivel nivel;
 
+    //construtor vazio para o JavaFX
     public Idioma () {
     }
 
+    //construtor completo para instanciar objetos do tipo Idioma
     public Idioma (String idioma, String nivelStr) {
         setIdioma(idioma);
         setNivel(nivelStr);
     }
 
+    //enumeração de valores fixos (constantes) para a situação do curso utilizando a classe Enum
     public enum Nivel {
         BASICO,
         INTERMEDIARIO,
         AVANCADO
     }
 
+    //setter do idioma com validação contra string nula ou vazia
     public void setIdioma (String idioma) {
         if (idioma != null && !idioma.isBlank()) {
             this.idioma = idioma;
         }
     }
 
+    /*setter do nivel de habilidade. recebe uma string, deixa-a em letras maiúsculas e converte para o tipo Enum e atribui
+    ao atributo Enum nivel*/
     public void setNivel (String nivelStr) {
         try{
             this.nivel = Nivel.valueOf(nivelStr.toUpperCase());
