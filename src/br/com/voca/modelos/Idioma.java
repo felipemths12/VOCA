6dbf2e77245a1cc2bbcf2929dc9d5deaefe238ab
Felipe <felipe.matheussms@gmail.com>
2025-05-10 15:33:01 -0300

    criando a classe idioma

--- src/br/com/voca/modelos/Idioma.java
+++ src/br/com/voca/modelos/Idioma.java
@@ -1,4 +1,35 @@
 package br.com.voca.modelos;
 
 public class Idioma {
+    private String idioma;
+    private String nivelStr;
+    private Nivel nivel;
+
+    public Idioma () {
+    }
+
+    public Idioma (String idioma, String nivelStr) {
+        setIdioma(idioma);
+        setNivel(nivelStr);
+    }
+
+    public enum Nivel {
+        BASICO,
+        INTERMEDIARIO,
+        AVANCADO
+    }
+
+    public void setIdioma (String idioma) {
+        if (idioma != null && !idioma.isBlank()) {
+            this.idioma = idioma;
+        }
+    }
+
+    public void setNivel (String nivelStr) {
+        try{
+            this.nivel = Nivel.valueOf(nivelStr.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            //implementar o tratamento no front-end
+        }
+    }
 }
